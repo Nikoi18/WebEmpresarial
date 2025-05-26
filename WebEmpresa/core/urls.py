7ef0fedcaefe6f887fef2fe4c6d7ccb6339d6ccb
Nicolas A. Urbaez A. <aran.nick15@gmail.com>
2025-05-26 18:34:10 +0200

    feature:PriemraApp realizada

---
+++ WebEmpresa/core/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from core import views as core_views
+
+urlpatterns = [
+    path('', core_views.home, name='home'),
+    path('about/', core_views.about, name='about'),
+    path('store/', core_views.store, name='store'),
+    path('services/', core_views.services, name='services'),    
+    path('contact/', core_views.contact, name='contact'),
+    path('blog/', core_views.blog, name='blog'), 
+    path('sample/', core_views.sample, name='sample'), 
+      
+]
